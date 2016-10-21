c7421b4ae70ba56fade49509be90e94f7d4f58d8
vbogdana <bogdana.veselinovic@yahoo.com>
2016-10-21 12:32:30 +0200

    master blade

---
+++ resources/views/home.blade.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+
+@extends('layouts.master')
+
+@section('scripts')
+<!-- ubaciti sve css i js fajlove potrebne za home page -->
+@stop
+
+@section('content')
+<!-- za sve ove klase treba napisati css -->
+<div class="video-section">
+    
+</div>
+
+<div class="about-us-section">
+    
+</div>
+
+<div class="packages-section">
+    
+</div>
+
+<div class="benefits-section">
+    
+</div>
+
+<div class="events-section">
+    
+</div>
+
+<div class="social-section">
+    
+</div>
+
+@stop
