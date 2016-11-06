27f0bae0d278c3e09803373c40d25b78c96458f2
vbogdana <bogdana.veselinovic@yahoo.com>
2016-11-06 17:02:44 +0100

    about us part 3

--- public/js/_main.js
+++ public/js/_main.js
@@ -41,7 +41,7 @@ $(window).on("load", function() {
             var element_bottom_position = (element_top_position + element_height);
 
             //check to see if this current container is within viewport
-            if ((element_bottom_position >= window_top_position) &&
+            if ((element_bottom_position >= (window_top_position)) &&
                 (element_top_position <= window_bottom_position)) {
                 $element.addClass('in-view');
             } else {
